19c290feebf46020fcb1eccfda243ef626eaf1bc
Marina Barbosa <mb.americo@unesp.br>
2024-08-27 22:17:12 -0300

    240827

--- mybc/parser.c
+++ mybc/parser.c
@@ -12,7 +12,12 @@ oplus = '+' || '-'
 // E -> [oplus] T R
 // oplus = '+' || '-'
 void E(void) { 
+    int oplus = 0;
+    int signal = 0;
+    int otimes = 0;
+
     if(lookahead == '+' || lookahead == '-') {
+        signal = lookahead;
         match(lookahead);
     }
 
@@ -27,24 +32,47 @@ _F:
             match(')');
             break;
         case DEC:
+            printf(" dec ");
             match(DEC);
             break;
         case OCT:
+            printf(" oct ");
             match(OCT);
             break;
         case HEX:
+            printf(" hex ");
             match(HEX);
             break;
         default:
+            printf(" id ");
             match(ID);
+    }
+    if(otimes) {
+        printf(" %c ", otimes);
+        otimes = 0;
     }
     // Q -> {otimes F }
     // otimes = '*' || '/'
-    if (lookahead == '*' || lookahead == '/') { match(lookahead); goto _F; }
-
+    if (lookahead == '*' || lookahead == '/') { 
+        otimes = lookahead;
+        match(lookahead); 
+        goto _F; 
+    }
+    if(signal) {
+        printf(" %c ", signal);
+        signal = 0;
+    }
+    if(oplus) {
+        printf(" %c ", oplus);
+        oplus = 0;
+    }
     // R -> {oplus T }
     // oplus = '+' || '-'
-    if (lookahead == '+' || lookahead == '-') { match(lookahead); goto _T; }
+    if (lookahead == '+' || lookahead == '-') { 
+        oplus = lookahead;
+        match(lookahead); 
+        goto _T; 
+    }
 }
 
 void match(int expected)
